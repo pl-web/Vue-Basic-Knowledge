8f0d9717f55a54e23dd1051f9279f473414ee75e
it-reader <hnature@126.com>
2020-09-24 08:09:58 +0800

    vue-snippets

---
+++ 01/01.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "",
+    data() {
+      return {
+        key: value
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
